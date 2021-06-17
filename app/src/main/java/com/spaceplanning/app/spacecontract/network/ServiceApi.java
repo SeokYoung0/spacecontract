b1987ae0293758478748c3c1314b845829a490eb
SeokYoung0 <darkmoon9507@naver.com>
2021-06-17 10:21:33 +0900

    GetFileActivity 생성 및 파일 첨부 기능 및 서버에 파일 전송 과정중 문제 발생. 서버에 파일을 보내려고하니 파일을 첨부하기에서 막힘

--- app/src/main/java/com/spaceplanning/app/spacecontract/network/ServiceApi.java
+++ app/src/main/java/com/spaceplanning/app/spacecontract/network/ServiceApi.java
@@ -3,11 +3,20 @@ package com.spaceplanning.app.spacecontract.network;
 import com.google.android.gms.auth.TokenData;
 
 
+import javax.xml.transform.Result;
+
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface  ServiceApi {
     @POST("/send/contract/")
     Call<ContractResponse> userLogin(@Body ContractData data);
+
+    @Multipart
+    @POST("/api/upload/")
+    Call<Result> uploadImage(@Part MultipartBody.Part File);
 }
